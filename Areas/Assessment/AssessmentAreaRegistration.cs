b3b193cebe58b477a343b25dcc444a8e3d662559
Jignesh Shah <jigsh@hotmail.com>
2013-06-10 23:42:37 -0400

    New Changes

---
+++ Areas/Assessment/AssessmentAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MSI.CCAT.WEB.Areas.Assessment
+{
+    public class AssessmentAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Assessment";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Assessment_default",
+                "Assessment/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[]{"MSI.CCAT.WEB.Areas.Assessment.Controllers"}
+            );
+        }
+    }
+}
